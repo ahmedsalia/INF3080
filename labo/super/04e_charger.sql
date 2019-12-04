476c140ec7b94589fe48b4e71c07dbf3ce34baa3
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2019-12-04 16:01:46 -0500

    maj

--- labo/super/04e_charger.sql
+++ labo/super/04e_charger.sql
@@ -5,7 +5,7 @@ BEGIN
 delete from Employe;
 commit;
 for s in 1..1500 LOOP 
-     insert into Employe(pEmploye,cEmploye)values(s, ' Emp');
+     insert into Employe(pEmploye,cEmploye)values(s, 'Emp ' || TO_CHAR(s));
    END LOOP;
  COMMIT;
 END;
