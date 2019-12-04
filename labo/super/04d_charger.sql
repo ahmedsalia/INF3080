cd7399895b0e0d0ab595cbf9ab6abfcd72558f82
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2019-12-04 16:20:49 -0500

    maj

--- labo/super/04d_charger.sql
+++ labo/super/04d_charger.sql
@@ -12,15 +12,15 @@ DECLARE
 BEGIN
  DELETE FROM Site;
  COMMIT;
- DELETE FROM Employe;
- COMMIT;
- FOR j IN 1..50 LOOP
-   INSERT INTO Employe  VALUES (j, TO_CHAR(j) || 'Employe');
- END LOOP;
- COMMIT;
+ --DELETE FROM Employe;
+ --COMMIT;
+ --FOR j IN 1..50 LOOP
+ --  INSERT INTO Employe  VALUES (j, TO_CHAR(j) || 'Employe');
+ --END LOOP;
+ --COMMIT;
  x := 0;
  FOR i IN 1..100 LOOP
-   SELECT MIN(pEmploye), MAX(pEmploye) INTO a, b FROM Employe;
+   SELECT MIN(pEmploye), 50 INTO a, b FROM Employe;
    FOR k IN a..b LOOP
      x := x + 1;
      INSERT INTO ServiceE (pServiceE, dServiceE, pEmploye) VALUES (x, TO_DATE('2015-06-12' , 'YYYY-MM-DD'), k);
