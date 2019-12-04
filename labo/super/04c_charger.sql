0e835d7a08bbbfcbd3157c68b8dd38faab6792c2
Ahmed Salia Toure <46532660+ahmedsalia@users.noreply.github.com>
2019-12-04 15:46:08 -0500

    Random NUMBERS

--- labo/super/04c_charger.sql
+++ labo/super/04c_charger.sql
@@ -2,21 +2,27 @@ DECLARE
  i NUMBER;
  k NUMBER;
  a NUMBER;
+ j NUMBER;
  x NUMBER;
+ p NUMBER;
+ o NUMBER;
+ y NUMBER;
+ z NUMBER;
  
 BEGIN
  DELETE FROM ServiceD;
  COMMIT;
-
- END LOOP;
- COMMIT;
  x := 0;
- a:= 10;
+ j:= 0 ;
+ SELECT MIN(pServiceE), MAX(pServiceE) INTO a, b FROM pServiceE;
+  SELECT MIN(pMachine), MAX(pMachine) INTO y, z FROM pMachine;
  FOR i IN 1..500 LOOP
    FOR k IN 1..10 LOOP
      x := x + 1;
-     a := a + 10;
-     INSERT INTO ServiceD (pServiceD, cDescription, nDuree, pMachine,pServiceE) VALUES (x, 'service', a,k,k);
+     j := FLOOR(RAND()*(10-5+1)+5);
+     p := FLOOR(RAND()*(b-a+1)+a);
+     o := FLOOR(RAND()*(z-y+1)+z);
+     INSERT INTO ServiceD (pServiceD, cDescription, nDuree, pMachine,pServiceE) VALUES (x, 'service', j,p,o);
    END LOOP;
  END LOOP;
  COMMIT;
