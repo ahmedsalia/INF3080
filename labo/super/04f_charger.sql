e3aa7452fcfee35496dbea10747db02dd3c8d159
ArmandBriere <46636172+ArmandBriere@users.noreply.github.com>
2019-12-04 15:45:43 -0500

    Update 04f_charger.sql

--- labo/super/04f_charger.sql
+++ labo/super/04f_charger.sql
@@ -3,10 +3,14 @@ i NUMBER;
 j NUMBER;
 v VARCHAR2(30);
 n INTEGER;
+pmod INTEGER;
+pre INTEGER;
+pty INTEGER;
+psi INTEGER;
 a NUMBER;
 k NUMBER;
- b NUMBER;
- x NUMBER;
+b NUMBER;
+x NUMBER;
 
 BEGIN
     DELETE FROM Machine;
@@ -22,26 +26,24 @@ BEGIN
     SELECT MIN(pVille) INTO a FROM ville;
     INSERT INTO Revision (pRevision, cRevision, cNote, dRevision) VALUES (1, 'Revision1', 'Grosse note', TO_DATE('1999-09-12', 'YYYY-MM-DD'));
 
-FOR i IN 1..5
-LOOP
-INSERT INTO typeMachine (pTypeMachine, cTypeMachine, bOption) VALUES (i, 'typeMachine' || i, MOD(i,2));
-INSERT INTO modele (pModele, cModele, dFinVie ) VALUES (i, 'modele' || i, SYSDATE + i);
-END LOOP;
-COMMIT;
+    FOR i IN 1..5 LOOP
+        INSERT INTO typeMachine (pTypeMachine, cTypeMachine, bOption) VALUES (i, 'typeMachine' || i, MOD(i,2));
+        INSERT INTO modele (pModele, cModele, dFinVie ) VALUES (i, 'modele' || i, SYSDATE + i);
+    END LOOP;
+    COMMIT;
 
-SELECT MIN(pVille), MAX(pVille) INTO a, b FROM ville;
-x := a;
+    SELECT MIN(pVille), MAX(pVille) INTO a, b FROM ville;
+    x := a;
 
-FOR i IN 1..15000
-LOOP
-    INSERT INTO Site (pSite, cAdresse, pVille) VALUES (i, i*22 || 'rue des Arbres', x );
-    IF x = b THEN
-        x := a;
-    ELSE  
-        x := (x+1);
-    END IF;
-END LOOP;
-
+    FOR i IN 1..15000 LOOP
+        INSERT INTO Site (pSite, cAdresse, pVille) VALUES (i, i*22 || 'rue des Arbres', x );
+        IF x = b THEN
+            x := a;
+        ELSE  
+            x := (x+1);
+        END IF;
+    END LOOP;
+    COMMIT;
     
     FOR j IN 1..50000 LOOP
         IF MOD(j,2) = 0 THEN
@@ -51,9 +53,11 @@ END LOOP;
         END IF;
 
         n := dbms_random.value(1000000000,2147483647);
-
+        pmod := dbms_random.value(1, 5);
+        pty  := dbms_random.value(1, 5);
+        psi  := dbms_random.value(1, 15000);
         INSERT INTO Machine (pMachine, cMachine, bActif, cSerie, dInstallation, pModele, pRevision, pTypeMachine, pSite) 
-        VALUES (j, v, 0, TO_CHAR(n), TO_DATE('1999-09-12', 'YYYY-MM-DD'), 1, 1, 1, 1);
+        VALUES (j, v, 0, TO_CHAR(n), TO_DATE('1999-09-12', 'YYYY-MM-DD'), pmod, 1, pty, psi);
     END LOOP;
     
     COMMIT;
