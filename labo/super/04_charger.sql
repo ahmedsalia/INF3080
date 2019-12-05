7aefe5a3d4113b1de11a36afd7d1f1300d530b22
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2019-12-05 10:36:15 -0500

    maj

--- labo/super/04_charger.sql
+++ labo/super/04_charger.sql
@@ -1,13 +1,13 @@
 --@04j_charger.sql
 --@04i_charger.sql
-@04h_charger.sql
-@04mk_charger.sql
+@04h_charger.sql (inclus i et j)
+@04mk_charger.sql (inclus m et k)
 --@04l_charger.sql
 --@04m_charger.sql
 @04e_charger.sql
-@04g_charger.sql
+--@04g_charger.sql (n,exsite pas)
 @04b_charger.sql (non fonctionnelle)
-@04f_charger.sql 
+@04f_charger.sql (inclus f, g et l ) a revoir danger avec ville, ne pas effacer (modele et typemachine)!
 @04d_charger.sql
 @04c_charger.sql
 --
