1b8e497465f0b3163fe3fcea95b5719e8caead82
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2019-12-05 10:19:15 -0500

    commentaires

--- labo/super/04_charger.sql
+++ labo/super/04_charger.sql
@@ -6,9 +6,9 @@
 --@04m_charger.sql
 @04e_charger.sql
 @04g_charger.sql
-@04b_charger.sql
-@04f_charger.sql
+@04b_charger.sql (non fonctionnelle)
+@04f_charger.sql 
 @04d_charger.sql
 @04c_charger.sql
 --
-@04a_charger.sql
+@04a_charger.sql (a faire)
